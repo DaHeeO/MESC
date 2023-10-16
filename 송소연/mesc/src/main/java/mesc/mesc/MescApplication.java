dbe0afd5e79db2e5762d8162c4fa336777b5b1e0
soyeon Song <songsoy95@gmail.com>
2023-10-16 22:35:27 +0900

    feat : 설정파일 및 CustomException 설정

---
+++ 송소연/mesc/src/main/java/mesc/mesc/MescApplication.java
@@ -0,0 +1,13 @@
+package mesc.mesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MescApplication.class, args);
+	}
+
+}
---
+++ 송소연/mesc/src/main/java/mesc/mesc/global/error/exception/BusinessException.java
@@ -0,0 +1,23 @@
+package mesc.mesc.global.error.exception;
+
+import mesc.mesc.global.error.ErrorCode;
+
+public class BusinessException extends RuntimeException {
+
+    private ErrorCode errorCode;
+
+    public BusinessException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+}
