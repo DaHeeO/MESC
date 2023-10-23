c81519d9fc38174ef50a0dd99149cc728376c60e
jihee9945 <jihee9945@naver.com>
2023-10-23 10:52:49 +0900

    Feat: add project

---
+++ mesc/src/test/java/com/ksol/mesc/MescApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksol.mesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
