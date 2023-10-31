c1c0d7ecb3a8c8322078c106f23902ee902777db
송소연 <songsoy95@gmail.com>
2023-10-31 15:42:42 +0900

    feat : 다중 이메일 발송 S09P31S105-259

--- mesc/src/main/java/com/ksol/mesc/domain/user/service/UserServiceImpl.java
+++ mesc/src/main/java/com/ksol/mesc/domain/user/service/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ksol.mesc.domain.user.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -7,7 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.ksol.mesc.domain.user.dto.LoginReq;
+import com.ksol.mesc.domain.user.dto.SendEmailReq;
+import com.ksol.mesc.domain.user.exception.EmailMessagingException;
 
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 public class UserServiceImpl implements UserService {
 
 	private final WebClient webClient;
+	private final EmailService emailService;
 
 	public ResponseEntity<?> forwardLoginRequest(LoginReq loginReq) {
 		return webClient.post()
@@ -27,6 +34,15 @@ public class UserServiceImpl implements UserService {
 						.retrieve()
 						.toEntity(String.class)
 						.block();
+	}
+
+	@Override
+	public void sendEmail(SendEmailReq sendEmailReq) {
+		try {
+			emailService.sendEmail(sendEmailReq.getEmails(), sendEmailReq.getSubject(), sendEmailReq.getContent());
+		} catch (MessagingException e) {
+			throw new EmailMessagingException("Failed To Send Email");
+		}
 	}
 
 }
