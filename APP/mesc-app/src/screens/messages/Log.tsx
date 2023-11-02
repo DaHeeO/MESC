3863fff007fbe154524e8fcb48b79d5c546cebba
Jaei <dobbykim0320@gmail.com>
2023-11-02 14:25:38 +0900

    feat : log navigation 설정 S09P31S105-273

---
+++ APP/mesc-app/src/screens/messages/Log.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// interface MessagesProps {
+//   navigation: any;
+// }
+
+const Log = () => {
+  return (
+    <View>
+      <Text>Log</Text>
+    </View>
+  );
+};
+export default Log;
---
+++ APP/mesc-app/src/screens/messages/Stack.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Messages from './Messages';
+import Log from './Log';
+
+const Stack = createStackNavigator();
+
+function MessageStack() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Messages">
+        <Stack.Screen
+          name="Messages"
+          component={Messages}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Log"
+          component={Log}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MessageStack;
