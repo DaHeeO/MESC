b462c208ab316a490ff1d83ba533ba34162216d6
이덕용 <leedeok1996@naver.com>
2023-11-07 15:00:53 +0900

    feat : 바로가기 버튼 엔티티 수정

--- BE/mesc/src/main/java/com/ksol/mesc/domain/component/type/directbutton/DirectButton.java
+++ BE/mesc/src/main/java/com/ksol/mesc/domain/component/type/directbutton/DirectButton.java
@@ -1,20 +1,14 @@
 package com.ksol.mesc.domain.component.type.directbutton;
 
+import com.ksol.mesc.domain.block.entity.Block;
 import com.ksol.mesc.domain.component.LinkType;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
 @Getter
-@Table(name = "DIRECT_CONNECT_BUTTON")
+@Table(name = "DIRECT_CONNECTION_BUTTON")
 public class DirectButton {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,14 +16,35 @@ public class DirectButton {
 	private Integer id;
 	@Column(name = "DCB_NAME")
 	private String name;
-	@Column(name = "RESPONSE_TYPE")
-	private String responseType;
-	private String response;
-	@Column(name = "DCBB_SEQUENCE")
-	private Integer sequence;
 	@Column(name = "DCB_LINK")
 	private String link;
 	@Column(name = "DCB_LINK_TYPE")
 	@Enumerated(EnumType.STRING)
 	private LinkType linkType;
+	@Column(name = "DCB_SEQUENCE")
+	private Integer sequence;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "BLOCK_ID")
+	private Block block;
+	@Column(name = "ICON_ID")
+	private Integer iconId;
+	@Column(name = "RESPONSE")
+	private String response;
+	@Column(name = "RESPONSE_TYPE")
+	private String responseType;
+
+	@Override
+	public String toString() {
+		return "DirectButton{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", link='" + link + '\'' +
+				", linkType=" + linkType +
+				", sequence=" + sequence +
+				", block=" + block.getId() +
+				", iconId=" + iconId +
+				", response='" + response + '\'' +
+				", responseType='" + responseType + '\'' +
+				'}';
+	}
 }
