28af57abe752ec5ccf11a9d700881fce8b69084d
jihee9945 <jihee9945@naver.com>
2023-11-19 16:57:30 +0900

    feat: actionService 조회 기능 추가

---
+++ BE/mesc/src/main/java/com/ksol/mesc/domain/action/controller/ActionController.java
@@ -0,0 +1,30 @@
+package com.ksol.mesc.domain.action.controller;
+
+import java.util.LinkedHashMap;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ksol.mesc.domain.action.service.ActionService;
+import com.ksol.mesc.domain.common.dto.response.CommonResponseDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/action")
+public class ActionController {
+	private final ActionService actionService;
+
+	@Operation(summary = "액션맵 조회 API", description = "액션맵을 DB에서 조회한다.")
+	@GetMapping
+	public ResponseEntity<CommonResponseDto<?>> selectActionMap() {
+		LinkedHashMap<String, Object> responseMap = actionService.selectActionMap();
+
+		return ResponseEntity.ok(CommonResponseDto.success(responseMap));
+	}
+
+}
---
+++ BE/mesc/src/main/java/com/ksol/mesc/domain/action/dto/response/ActionRes.java
@@ -0,0 +1,9 @@
+package com.ksol.mesc.domain.action.dto.response;
+
+import lombok.Data;
+
+@Data
+public class ActionRes {
+	private Integer id;
+	private String query;
+}
