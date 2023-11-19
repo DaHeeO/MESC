50a56c141a6d6180dbf4c4d93daaae3f96a85fd7
손승연 <zzangookd@gmail.com>
2023-11-20 04:46:06 +0900

    feat: 리스트 추가 삭제

--- WEB/mesc-web/src/App.tsx
+++ WEB/mesc-web/src/App.tsx
@@ -1,39 +1,36 @@
-import { Route, Routes } from "react-router-dom";
-import { RecoilRoot } from "recoil";
-import { Navigate } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { Navigate } from 'react-router-dom';
 // Header
-import Layout from "./pages/layout";
+import Layout from './pages/layout';
 // Subpages
-import { MainPage } from "./pages/main/MainPage";
-import { Add } from "./pages/AddBlock/Add";
-import { Modify } from "./pages/updateCard/Modify";
-import Menu3 from "./pages/menu3";
-import { Faq } from "./pages/FAQ/Faq";
+import { MainPage } from './pages/main/MainPage';
+import { Add } from './pages/AddBlock/Add';
+import { Modify } from './pages/updateCard/Modify';
+import { Menu3 } from './pages/List/Menu3';
+import { Faq } from './pages/FAQ/Faq';
 // Layout excluding page
-import Login from "./pages/login/login";
+import Login from './pages/login/login';
 
 function App() {
-  return (
-    <>
-      <RecoilRoot>
-        <Routes>
-          <Route path="/Login" element={<Login />} />
-          <Route path="/*" element={<Layout />}>
-            <Route path="Add" element={<Add />} />
-            <Route path="Modify/:id" element={<Modify />} />
-            {/* <Route path="Modify" element={<Modify />} /> */}
-            <Route path="Menu3" element={<Menu3 />} />
-            <Route path="Faq" element={<Faq />} />
-            <Route path="" element={<MainPage />} />
-            <Route
-              path="Modify/*"
-              element={<Navigate replace to="../Modify/1" />}
-            />
-          </Route>
-        </Routes>
-      </RecoilRoot>
-    </>
-  );
+    return (
+        <>
+            <RecoilRoot>
+                <Routes>
+                    <Route path='/Login' element={<Login />} />
+                    <Route path='/*' element={<Layout />}>
+                        <Route path='Add' element={<Add />} />
+                        <Route path='Modify/:id' element={<Modify />} />
+                        {/* <Route path="Modify" element={<Modify />} /> */}
+                        <Route path='Menu3' element={<Menu3 />} />
+                        <Route path='Faq' element={<Faq />} />
+                        <Route path='' element={<MainPage />} />
+                        <Route path='Modify/*' element={<Navigate replace to='../Modify/1' />} />
+                    </Route>
+                </Routes>
+            </RecoilRoot>
+        </>
+    );
 }
 
 export default App;
--- WEB/mesc-web/src/component/common/Layout/layout/sideBar.tsx
+++ WEB/mesc-web/src/component/common/Layout/layout/sideBar.tsx
@@ -1,169 +1,130 @@
-import { useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { AboutContainer } from "../../About/AboutContainer";
-import * as C from "../../theme";
-import * as S from "./sideBar.styles";
+import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { AboutContainer } from '../../About/AboutContainer';
+import * as C from '../../theme';
+import * as S from './sideBar.styles';
 
-import HomeFocus from "../../../../assets/icon/homeFocus.svg";
-import AddFocus from "../../../../assets/icon/addFocus.svg";
-import EditFocus from "../../../../assets/icon/editFocus.svg";
-import FaqFocus from "../../../../assets/icon/faqFocus.svg";
-import LogoutFocus from "../../../../assets/icon/logoutFocus.svg";
-import Home from "../../../../assets/icon/home.svg";
-import Add from "../../../../assets/icon/add.svg";
-import Edit from "../../../../assets/icon/edit.svg";
-import Faq from "../../../../assets/icon/faq.svg";
-import Logout from "../../../../assets/icon/logout.svg";
+import HomeFocus from '../../../../assets/icon/homeFocus.svg';
+import AddFocus from '../../../../assets/icon/addFocus.svg';
+import EditFocus from '../../../../assets/icon/editFocus.svg';
+import FaqFocus from '../../../../assets/icon/faqFocus.svg';
+import LogoutFocus from '../../../../assets/icon/logoutFocus.svg';
+import Home from '../../../../assets/icon/home.svg';
+import Add from '../../../../assets/icon/add.svg';
+import Edit from '../../../../assets/icon/edit.svg';
+import Faq from '../../../../assets/icon/faq.svg';
+import Logout from '../../../../assets/icon/logout.svg';
 
 function SideBar() {
-  const [hovered, setHovered] = useState(false);
-  const navigate = useNavigate();
-  const location = useLocation();
+    const [hovered, setHovered] = useState(false);
+    const navigate = useNavigate();
+    const location = useLocation();
 
-  const goMain = () => {
-    navigate("/");
-  };
-  const goAdd = () => {
-    navigate("/Add");
-  };
-  const goMenu2 = () => {
-    navigate("/Modify");
-  };
+    const goMain = () => {
+        navigate('/');
+    };
+    const goAdd = () => {
+        navigate('/Add');
+    };
+    const goMenu2 = () => {
+        navigate('/Modify');
+    };
 
-  const goMenu4 = () => {
-    navigate("/Faq");
-  };
+    const goMenu4 = () => {
+        navigate('/Faq');
+    };
+    const goMenu5 = () => {
+        navigate('/Menu3');
+    };
 
-  const goLogout = () => {
-    localStorage.clear();
-    alert("로그아웃 되었습니다.");
-    navigate("/login");
-  };
+    const goLogout = () => {
+        localStorage.clear();
+        alert('로그아웃 되었습니다.');
+        navigate('/login');
+    };
 
-  const isActive = (path: string) => {
-    if (location.pathname === path) {
-      return true;
-    }
+    const isActive = (path: string) => {
+        if (location.pathname === path) {
+            return true;
+        }
 
-    return location.pathname.startsWith(path + "/");
-  };
+        return location.pathname.startsWith(path + '/');
+    };
 
-  return (
-    // {/* sideBar */}
-    <AboutContainer
-      height="100%"
-      width="20%"
-      flexDirection="column"
-      justifyContent="flex-start"
-      align="flex-end"
-      style={{
-        borderRight: "1px solid #dddddd",
-        backgroundColor: C.colors.samsungBlue,
-      }}
-    >
-      <S.HeaderDiv>
-        <S.HeaderLogoDiv>
-          <S.Logo onClick={goMain}>
-            <S.Text size={22} color={C.colors.samsungBlue} weight={900}>
-              M
-            </S.Text>
-          </S.Logo>
-          <S.LogoBox onClick={goMain}>
-            <S.Text size={18} color="white" weight={800}>
-              MESC
-            </S.Text>
-            <S.Text size={10} color="white">
-              ADMIN
-            </S.Text>
-          </S.LogoBox>
-        </S.HeaderLogoDiv>
-      </S.HeaderDiv>
-      <S.BodyDiv>
-        <S.MainDiv>
-          <S.NavContainer onClick={goMain}>
-            <img
-              width={18}
-              height={18}
-              src={isActive("/") ? HomeFocus : Home}
-            />
-            <S.Text
-              size={14}
-              color={isActive("/") ? "white" : C.colors.textIcyGray}
-              weight={500}
-              style={{ paddingLeft: "14px" }}
-            >
-              홈
-            </S.Text>
-          </S.NavContainer>
-          <S.NavContainer onClick={goAdd}>
-            <img
-              width={18}
-              height={18}
-              src={isActive("/Add") ? AddFocus : Add}
-            />
-            <S.Text
-              size={14}
-              color={isActive("/Add") ? "white" : C.colors.textIcyGray}
-              weight={500}
-              style={{ paddingLeft: "14px" }}
-            >
-              블록 추가하기
-            </S.Text>
-          </S.NavContainer>
-          <S.NavContainer onClick={goMenu2}>
-            <img
-              width={18}
-              height={18}
-              src={isActive("/Modify") ? EditFocus : Edit}
-            />
-            <S.Text
-              size={14}
-              color={isActive("/Modify") ? "white" : C.colors.textIcyGray}
-              weight={500}
-              style={{ paddingLeft: "14px" }}
-            >
-              블록 수정하기
-            </S.Text>
-          </S.NavContainer>
-          <S.NavContainer onClick={goMenu4}>
-            <img
-              width={18}
-              height={18}
-              src={isActive("/Faq") ? FaqFocus : Faq}
-            />
-
-            <S.Text
-              size={14}
-              color={isActive("/Faq") ? "white" : C.colors.textIcyGray}
-              weight={500}
-              style={{ paddingLeft: "14px" }}
-            >
-              FAQ
-            </S.Text>
-          </S.NavContainer>
-        </S.MainDiv>
-        <S.LogoutContainer
-          onClick={goLogout}
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
+    return (
+        // {/* sideBar */}
+        <AboutContainer
+            height='100%'
+            width='20%'
+            flexDirection='column'
+            justifyContent='flex-start'
+            align='flex-end'
+            style={{
+                borderRight: '1px solid #dddddd',
+                backgroundColor: C.colors.samsungBlue,
+            }}
         >
-          {hovered ? (
-            <img width={18} height={18} src={LogoutFocus} />
-          ) : (
-            <img width={18} height={18} src={Logout} />
-          )}
-          <S.Text
-            size={14}
-            color={hovered ? "white" : C.colors.textIcyGray}
-            weight={500}
-            style={{ paddingLeft: "14px", cursor: "pointer" }}
-          >
-            로그아웃
-          </S.Text>
-        </S.LogoutContainer>
-      </S.BodyDiv>
-    </AboutContainer>
-  );
+            <S.HeaderDiv>
+                <S.HeaderLogoDiv>
+                    <S.Logo onClick={goMain}>
+                        <S.Text size={22} color={C.colors.samsungBlue} weight={900}>
+                            M
+                        </S.Text>
+                    </S.Logo>
+                    <S.LogoBox onClick={goMain}>
+                        <S.Text size={18} color='white' weight={800}>
+                            MESC
+                        </S.Text>
+                        <S.Text size={10} color='white'>
+                            ADMIN
+                        </S.Text>
+                    </S.LogoBox>
+                </S.HeaderLogoDiv>
+            </S.HeaderDiv>
+            <S.BodyDiv>
+                <S.MainDiv>
+                    <S.NavContainer onClick={goMain}>
+                        <img width={18} height={18} src={isActive('/') ? HomeFocus : Home} />
+                        <S.Text size={14} color={isActive('/') ? 'white' : C.colors.textIcyGray} weight={500} style={{ paddingLeft: '14px' }}>
+                            홈
+                        </S.Text>
+                    </S.NavContainer>
+                    <S.NavContainer onClick={goAdd}>
+                        <img width={18} height={18} src={isActive('/Add') ? AddFocus : Add} />
+                        <S.Text size={14} color={isActive('/Add') ? 'white' : C.colors.textIcyGray} weight={500} style={{ paddingLeft: '14px' }}>
+                            블록 추가하기
+                        </S.Text>
+                    </S.NavContainer>
+                    <S.NavContainer onClick={goMenu2}>
+                        <img width={18} height={18} src={isActive('/Modify') ? EditFocus : Edit} />
+                        <S.Text size={14} color={isActive('/Modify') ? 'white' : C.colors.textIcyGray} weight={500} style={{ paddingLeft: '14px' }}>
+                            블록 수정하기
+                        </S.Text>
+                    </S.NavContainer>
+                    <S.NavContainer onClick={goMenu5}>
+                        <img width={18} height={18} src={isActive('/Menu3') ? FaqFocus : Faq} />
+
+                        <S.Text size={14} color={isActive('/Menu3') ? 'white' : C.colors.textIcyGray} weight={500} style={{ paddingLeft: '14px' }}>
+                            리스트
+                        </S.Text>
+                    </S.NavContainer>
+                    <S.NavContainer onClick={goMenu4}>
+                        <img width={18} height={18} src={isActive('/Faq') ? FaqFocus : Faq} />
+
+                        <S.Text size={14} color={isActive('/Faq') ? 'white' : C.colors.textIcyGray} weight={500} style={{ paddingLeft: '14px' }}>
+                            FAQ
+                        </S.Text>
+                    </S.NavContainer>
+                </S.MainDiv>
+                <S.LogoutContainer onClick={goLogout} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                    {hovered ? <img width={18} height={18} src={LogoutFocus} /> : <img width={18} height={18} src={Logout} />}
+                    <S.Text size={14} color={hovered ? 'white' : C.colors.textIcyGray} weight={500} style={{ paddingLeft: '14px', cursor: 'pointer' }}>
+                        로그아웃
+                    </S.Text>
+                </S.LogoutContainer>
+            </S.BodyDiv>
+        </AboutContainer>
+    );
 }
 
 export default SideBar;
