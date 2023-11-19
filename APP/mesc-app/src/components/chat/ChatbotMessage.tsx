20f659d7237c08a572cb3de8d3849ce9a2a76c2b
송소연 <songsoy95@gmail.com>
2023-11-20 03:46:08 +0900

    fix : 채팅방 디테일 수정

--- APP/mesc-app/src/components/chat/ChatbotMessage.tsx
+++ APP/mesc-app/src/components/chat/ChatbotMessage.tsx
@@ -22,7 +22,7 @@ function ChatbotMessage(props: {card: Card}) {
       context !== undefined &&
       context.includes('SQLSyntaxErrorException')
     ) {
-      setTextStyle({color: 'red'});
+      setTextStyle({color: '#cb2727'});
     } else {
       setTextStyle({color: '#323639'}); // 기본 색상
     }
@@ -52,8 +52,8 @@ function ChatbotMessage(props: {card: Card}) {
             style={{
               fontSize: 14,
               textAlign: 'left',
-              color: '#323639',
               fontWeight: 'bold',
+              ...textStyle,
             }}>
             {context}
           </Text>
